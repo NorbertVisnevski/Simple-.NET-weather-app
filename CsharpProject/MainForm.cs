3d5fecdcdc65648ee1e1044bef0a97a51e05fdb2
Norbert Visnevski <norbert.wiszniewski2000@gmail.com>
2021-07-28 20:01:14 +0300

    Minor changes

--- CsharpProject/MainForm.cs
+++ CsharpProject/MainForm.cs
@@ -38,6 +38,16 @@ namespace CsharpProject
             settingsCountryCodeTextField.Text = Store.defaultCountry;
             loadCurrent(Store.defaultCity, Store.defaultCountry);
         }
+        private void MainForm_Resize(object sender, System.EventArgs e)
+        {
+            Control control = (Control)sender;
+
+            // Ensure the Form remains square (Height = Width).
+            if (control.Size.Height != control.Size.Width)
+            {
+                control.Size = new Size(control.Size.Width, control.Size.Width);
+            }
+        }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# .NET project
+# Simple .NET weather app
+
+This just a siple project which I made in like a day for my university assignment.
+
+![Main screen](https://imgur.com/c9cWMbe)
+![Weekly weather](https://imgur.com/LHXHSpK)
+![Settings](https://imgur.com/K2Nj94o)
